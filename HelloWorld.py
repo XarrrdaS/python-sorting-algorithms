67f57e971e7b440c066e81f4fdeb6ed14d6804f9
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-17 16:11:06 +0100

    działa

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,3 +4,4 @@ def HelloWorld():
     return f"Hello World! I would like to check if the github is working properly. Anyway, today is {date.today()}!"
 #Insertion sort, Shell sort, Selection sort, Heap sort, Quick sort (random pivot, left pivot)
 #test, czy visualka ogarnia
+#super
