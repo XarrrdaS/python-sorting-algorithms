3a194c1ffca679305c2dec871409b977a6fa671e
XarrrdaS <k.radomski1226@gmail.com>
2024-03-18 19:28:23 +0100

    QuickSort_leftPivot - done

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from algorithms.QuickSort_leftPivot import quickSort_leftPivot
 
 #Insertion sort, Shell sort, Selection sort, Heap sort, Quick sort (random pivot, left pivot)
 if __name__ == "__main__":
-    array_quickSortLeftPivot = [2,6,8,10,1,2,1]
-    print(quickSort_leftPivot(array_quickSortLeftPivot))
+    testArray_quickSortLeftPivot = [2,6,8,10,1,2,1]
+    print(quickSort_leftPivot(testArray_quickSortLeftPivot))
     
