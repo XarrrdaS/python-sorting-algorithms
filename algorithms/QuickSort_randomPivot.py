9a22dfca3601a9cbee08df48900173d6fcba52c4
XarrrdaS <k.radomski1226@gmail.com>
2024-03-18 20:43:55 +0100

    QuickSort_randomPivot - done

--- algorithms/QuickSort_randomPivot.py
+++ algorithms/QuickSort_randomPivot.py
@@ -1,1 +1,30 @@
+import random
 
+def quickSort_randomPivot(array):
+    # when none or only one element occured in the array just return it
+    # to be able to move on and select a pivot
+    if len(array) <= 1:
+        return array
+    else:
+        # take the random element as pivot
+        pivot = array[random.randint(0, len(array) - 1)]
+        larger = []
+        equalToPivot = []
+        smaller = []
+        # loop through all elements in the array
+        for i in array:
+            # if the element is larger than pivot, add it to the array in order to
+            # designate a new array of larger elements than pivot 
+            if i > pivot:
+                larger.append(i)
+            # same as above but with equal to pivot element
+            elif i == pivot:
+                equalToPivot.append(i)
+            # same as above but with smaller than pivot element
+            else:
+                smaller.append(i)
+        # recursive execution of function in order to divide arrays with smaller and larger
+        # numbers compared to current pivot, as at the beggining if the array length is lower than or equal to 1 
+        # it will return the exact element
+        return quickSort_randomPivot(smaller) + equalToPivot + quickSort_randomPivot(larger)
+    
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from algorithms.QuickSort_leftPivot import quickSort_leftPivot
+from algorithms.QuickSort_randomPivot import quickSort_randomPivot
 
 #Insertion sort, Shell sort, Selection sort, Heap sort, Quick sort (random pivot, left pivot)
 if __name__ == "__main__":
-    testArray_quickSortLeftPivot = [2,6,8,10,1,2,1]
+    testArray_quickSortLeftPivot = [2, 6, 8, 10, 1, 2, 1, 1, 1, 2, 10]
     print('QuickSort - left pivot:')
     print(quickSort_leftPivot(testArray_quickSortLeftPivot))
     
+    testArray_quickSortRandomPivot = [6, 5, 4, 6, 7, 3, 4, 6, 7, 4, 111, 12, 0]
+    print('\nQuickSort - random pivot:')
+    print(quickSort_randomPivot(testArray_quickSortRandomPivot))
