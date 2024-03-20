e7bc5f1be974290d8e504f5905f9fb066ed7a7fd
XarrrdaS <k.radomski1226@gmail.com>
2024-03-20 21:41:33 +0100

    final up

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 import sys
-from   algorithms.InsertionSort import insertion_sort
-from   algorithms.HeapSort import heapSort
-from   algorithms.QuickSort_leftPivot import quickSort_leftPivot
-from   algorithms.QuickSort_randomPivot import quickSort_randomPivot
-from   algorithms.SelectionSort import selection_sort
-from   algorithms.ShellSort import shell_sort
+from algorithms.InsertionSort import insertion_sort
+from algorithms.HeapSort import heapSort
+from algorithms.QuickSort_leftPivot import quickSort_leftPivot
+from algorithms.QuickSort_randomPivot import quickSort_randomPivot
+from algorithms.SelectionSort import selection_sort
+from algorithms.ShellSort import shell_sort
 
 
 def sort_using_algorithm(data, algorithm):
