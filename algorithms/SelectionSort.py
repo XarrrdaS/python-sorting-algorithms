a5be6b1e6e628dc93dbab0d042d10d8bb0ec51bb
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-20 20:07:18 +0100

    added sorting methods

--- algorithms/SelectionSort.py
+++ algorithms/SelectionSort.py
@@ -1,1 +1,14 @@
-#Hubert B)
+def selection_sort(array):
+    # loop through the array
+    for i in range(len(array)):
+        # define the minimum element
+        min_index = i
+        # loop through the array starting from the next element
+        for j in range(i + 1, len(array)):
+            # if the current element is less than the minimum element
+            if array[j] < array[min_index]:
+                # set the current element as the minimum element
+                min_index = j
+        # swap the elements
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
--- algorithms/ShellSort.py
+++ algorithms/ShellSort.py
@@ -1,1 +1,22 @@
-Tworzę
+def shell_sort(array):
+    # define the gap
+    gap = len(array) // 2
+    # loop through the array
+    while gap > 0:
+        # loop through the array starting from the gap
+        for i in range(gap, len(array)):
+            # define the current element
+            current = array[i]
+            # define the position of the element
+            position = i
+            # loop through the array starting from the position of the current element
+            while position >= gap and array[position - gap] > current:
+                # swap the elements
+                array[position] = array[position - gap]
+                # move the position
+                position -= gap
+            # set the current element to the position
+            array[position] = current
+        # divide the gap by 2
+        gap //= 2
+    return array
