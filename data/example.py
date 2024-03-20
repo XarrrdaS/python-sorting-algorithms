9262a6ec1971a35e232eaf6d945446a6445dd852
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-20 21:25:33 +0100

    final

--- data/example.py
+++ data/example.py
@@ -1,4 +1,11 @@
 import sys
+from   algorithms.InsertionSort import insertion_sort
+from   algorithms.HeapSort import heapSort
+from   algorithms.QuickSort_leftPivot import quickSort_leftPivot
+from   algorithms.QuickSort_randomPivot import quickSort_randomPivot
+from   algorithms.SelectionSort import selection_sort
+from   algorithms.ShellSort import shell_sort
+
 
 def sort_using_algorithm(data, algorithm):
     # This function takes the algorithm identifier as input
@@ -7,6 +14,22 @@ def sort_using_algorithm(data, algorithm):
     sorted_data = sorted(data)
 
     return sorted_data
+
+
+def chooseAlgorithm(data, algorithmNum):
+    if algorithmNum == 1:
+        insertion_sort(data)
+    if algorithmNum == 2:
+        shell_sort(data)
+    if algorithmNum == 3:
+        selection_sort(data)
+    if algorithmNum == 4:
+        heapSort(data)
+    if algorithmNum == 5:
+        quickSort_leftPivot(data)
+    if algorithmNum == 6:
+        quickSort_randomPivot(data)
+
 
 def main():
     # Command-line arguments: python script.py --algorithm <algorithm_number>
@@ -24,10 +47,10 @@ def main():
         print("Error reading input.")
 
     # Perform sorting using the specified algorithm (ignored in this example)
-    sorted_data = sort_using_algorithm(data, algorithm_number)
+    sorted_data = chooseAlgorithm(data, algorithm_number)
 
     # Print the sorted data
-    print("Sorted data:", sorted_data[0:10])
+    print("Sorted data:", sorted_data)
 
 if __name__ == "__main__":
     main()
