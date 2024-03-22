59b7dbdf2ee5535aa9ebf6ada814a12abd9cb873
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-22 13:50:17 +0100

    dodałem opisy

--- algorithms/ShellSort.py
+++ algorithms/ShellSort.py
@@ -1,22 +1,22 @@
 def shell_sort(array):
-    # define the gap
-    gap = len(array) // 2
+    # define the interval
+    interval = len(array) // 2
     # loop through the array
-    while gap > 0:
-        # loop through the array starting from the gap
-        for i in range(gap, len(array)):
+    while interval > 0:
+        # loop through the array starting from the interval
+        for i in range(interval, len(array)):
             # define the current element
             current = array[i]
             # define the position of the element
             position = i
             # loop through the array starting from the position of the current element
-            while position >= gap and array[position - gap] > current:
+            while position >= interval and array[position - interval] > current:
                 # swap the elements
-                array[position] = array[position - gap]
+                array[position] = array[position - interval]
                 # move the position
-                position -= gap
+                position -= interval
             # set the current element to the position
             array[position] = current
-        # divide the gap by 2
-        gap //= 2
+        # divide the interval by 2
+        interval //= 2
     return array
