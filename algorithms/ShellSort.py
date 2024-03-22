857aec03c7765b59956ba4c32fbc43eb37ffe2cc
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-22 14:42:53 +0100

    sedgewicktemp

--- algorithms/ShellSort.py
+++ algorithms/ShellSort.py
@@ -1,7 +1,13 @@
 def shell_sort(array):
     # define the interval
-    interval = len(array) // 2
+    sedgewick = [0] * round(len(array)/2)
+    for s in range(round(len(array)/2)):
+        sedgewick[s] = 4**(s+1) + 3*2**s + 1
+    interval = sedgewick[:-1]
+    # print(interval)
+    # interval = len(array) // 2
     # loop through the array
+    temp = round(len(array)/2)-1
     while interval > 0:
         # loop through the array starting from the interval
         for i in range(interval, len(array)):
@@ -18,5 +24,9 @@ def shell_sort(array):
             # set the current element to the position
             array[position] = current
         # divide the interval by 2
-        interval //= 2
+        temp = temp-1
+        interval = sedgewick[temp]
+        print(interval)
     return array
+
+print(shell_sort([5, 3, 8]))
