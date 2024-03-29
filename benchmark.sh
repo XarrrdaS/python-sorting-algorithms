fad2ff582408ee2780239cff04e52723002653d6
Hubert Rutkowski <hubert00rutkowski@gmail.com>
2024-03-29 01:21:33 +0100

    finished testing

--- benchmark.sh
+++ benchmark.sh
@@ -26,7 +26,12 @@ benchmark() {
 
 # Map algorithm names to numbers
 declare -A algorithm_mapping=(
-    ["quick_sort_left_pivot"]=1
+    ["insertion_sort"]=1
+    ["shell_sort"]=2
+    ["selection_sort"]=3
+    ["heap_sort"]=4
+    ["quick_sort_left_pivot"]=5
+    ["quick_sort_random_pivot"]=6
 )
 
 # Create a reverse mapping from numbers to algorithm names
@@ -39,7 +44,7 @@ done
 
 # List of input file types
 #input_files=("random_array" "increasing_array" "decreasing_array" "constant_array" "a_shaped_array")
-input_files=("increasing_array")
+input_files=("random_array")
 
 # Create or clear the CSV file
 echo "Algorithm,InputSize,Time" > benchmark_results.csv
